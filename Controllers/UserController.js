7687236b2caa6af0286b6c409419a2661d49d869
thakur06 <79101220+thakur06@users.noreply.github.com>
2023-07-03 19:36:17 +0530

    message

---
+++ Controllers/UserController.js
@@ -0,0 +1,21 @@
+const User=require('../Schema/UserModel');
+const login=async(req,res)=>{
+try {
+    
+} catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+}
+}
+
+const signIn=async(req,res)=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send(error);
+    }
+    };
+
+
+    module.exports ={login,signIn}
---
+++ Schema/UserModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstname: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+   profilePicture: String,
+    coverPicture: String,
+    about: String,
+    livesIn: String,
+    worksAt: String,
+    relationship: String,
+    country: String,
+    followers: [],
+    following: [],
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("Users", UserSchema);
+export default UserModel;
